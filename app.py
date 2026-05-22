f49dcde9fc3a4edce1084636cecc3aef15c6bca7
Shalma <shalmaahalida@gmail.com>
2026-05-22 07:58:52 +0700

    update UI

--- app.py
+++ app.py
@@ -2,14 +2,20 @@ from flask import Flask, render_template, request
 
 application = Flask(__name__)
 
-@application.route(rule='/', methods=['GET', 'POST'])
+@application.route('/', methods=['GET', 'POST'])
 def index():
+
     if request.method == 'POST':
+
         namaDepan = request.form['namaDepan']
         namaBelakang = request.form['namaBelakang']
-        nama = '%s %s' % (namaDepan, namaBelakang)
+
+        nama = namaDepan + " " + namaBelakang
+
         return render_template('response.html', nama=nama)
+
     return render_template('form.html')
+
 
 if __name__ == '__main__':
     application.run(debug=True)
